a2ed8fa7a1437781c7ab617f77c898316f305ebf
yukimatJP <tbonfukitama110@gmail.com>
2018-05-30 16:51:55 +0200

    make env with gulp

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+var gulp = require("gulp");
+var fs = require("fs");
+var ejs = require("gulp-ejs");
+var sass = require("gulp-sass");
+var uglify = require("gulp-uglify");
+var browser = require("browser-sync");
+var plumber = require("gulp-plumber");
+
+gulp.task("server", function() {
+    browser({
+        server: {
+            baseDir: "./docs",
+            startPath: '/perdial/',
+            routes : {
+                '/perdial': 'docs'
+            }
+        }
+    });
+});
+
+gulp.task("ejs", function() {
+    var json = JSON.parse(fs.readFileSync("./source/variables.json"));
+    gulp.src(["./source/**/*.ejs", "!./source/template/*.ejs"], { base: "./source" })
+        .pipe(plumber())
+        .pipe(ejs(json, {"ext": ".html"}))
+        .pipe(gulp.dest("./docs/"))
+        .pipe(browser.reload({stream:true}));
+});
+ 
+gulp.task("sass", function() {
+    gulp.src("./source/**/*.scss")
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(gulp.dest("./docs/"))
+        .pipe(browser.reload({stream:true}));
+});
+
+gulp.task("js", function() {
+    gulp.src(["./source/**/*.js"])
+        .pipe(plumber())
+        .pipe(uglify())
+        .pipe(gulp.dest("./docs/"))
+        .pipe(browser.reload({stream:true}));
+});
+
+gulp.task("reload", function() {
+    browser.reload();
+});
+
+gulp.task("default", ["server"], function() {
+    gulp.watch("./source/**/*.ejs", ["ejs"]);
+    gulp.watch("./source/**/*.js",  ["js"]);
+    gulp.watch("./source/**/*.scss",["sass"]);
+});
